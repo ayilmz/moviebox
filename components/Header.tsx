71b754dfac11b029dd5f85a49883e5ee57c157dc
ayilmz <ayilmaz@ebay.com>
2021-10-18 22:45:01 +0300

    completed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,18 @@ const Header = () => (
             <div className={styles.logo}>
                 <Image   src="/movieBox.png" alt="MovieBox" width={186} height={50} />
             </div>
+            <div className={styles.searchBar}>
+                <i className={styles.searchIcon}></i>
+                <input type="text" placeholder="What do you want to watch?"/>
+            </div>
+            <div className={styles.loginAndHamburgerMenu}>
+                <a href="#" className={styles.loginLink}>
+                    Sign in
+                </a>
+                <a href="#" className={styles.hamburgermenu}>
+
+                </a>
+            </div>
         </div>
 )
 
